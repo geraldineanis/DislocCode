73290da767b727e007606719732d7042b89877bc
Geraldine Anis <girawael@gmail.com>
2023-10-30 11:05:08 +0000

    dislocation tracking docs

--- dislocation_tracking/dislocation_script.py
+++ dislocation_tracking/dislocation_script.py
@@ -1,41 +1,62 @@
+"""!
+Dislocation trajectory post-processing script.
+
+@ref This script demonstrates how the output of the OVITO DXA tool, which is used in ovito_disloc.py, can be post-processed to "track" these dislocations throughout the trajectory. 
+The script ovito_disloc.py uses DXA to detect dislocations in Molecular Dynamics (MD) trajectories and outputs the coordinates of the vertices of each detected dislocation line.
+However, the OVITO DXA tool does not index detected dislocations consistently across frames i.e. one dislocation can be indexed as dislocation 0
+in one frame and dislocation 1 in a different frame. This makes it difficult to follow or "track" a specific dislocation across a trajectory for further analysis, such as studying its dynamics.
+
+@ref This script shows how this can be overcome by using the tools contained in dislocation_analysis.py to extract trajectories (position vs. time data) for each distinct dislocation detected by OVITOS'
+DXA. This script takes as input text files containing the output from DXA in the format described in the documentation of ovito_disloc.py.
+
+@ref This script is used to analyse an MD simulation of an a/2<110>{111} edge dislocation in pure Ni. This dislocation dissociates into two partial dislocations. The scripts lmp_pre_strain.py, gen_1disloc_Ni.py,
+and gen_111_atoms.py can be used to create the structure and run the MD simulation.
+
+This script can be used to analyse trajectories containing any number of dislocations (or partial dislocations), given the following assumptions:
+1. The dislocations are straight, edge dislocations.
+2. The dislocations only move in the x direction.
+3. The number of dislocations is constant across all frames
+i.e. no dislocations are generated nor lost throughout the analysed trajectory.
+
+Some plotting code is provided at the end of the script to illustrate its output. Two figures are generated each showing the average x-position vs. time and x-velocity vs. time plots for the:
+1. Partial disocations.
+2. Perfect dislocation (the average of the two partials)
+
+@sa track_disloc
+"""
+
 import numpy as np
 from matplotlib import pyplot as plt
 import glob
 from natsort import natsorted
 from dislocation_analysis import *
 
-###################################################################
-#                                                                 #
-#                              Analysis                           #
-#                                                                 #
-###################################################################
+# Post-processing OVITO DXA output
 
-# List frames
+# List MD trajectory frames sorted by frame number
+# The OVITO DXA output is stored in the disloc_data_*.txt files
+# The wildcard * indicates the frame number
 disloc_frames = natsorted(glob.glob("./Ni_disloc/disloc_data_*.txt"))
 
-step = 500*0.001
+# timestep = writing frequency * MD timestep
+timestep = 500*0.001
 
-# Dislocation lines coordinates
+# Simulation Timesteps
+sim_time = [i*timestep for i in range(len(disloc_frames))]
+
+# Arrays to store coordinates of dislocation lines
 x_coords = []
 y_coords = []
 z_coords = []
-
-# Time
-time = []
-for i in range(len(disloc_frames)):
-    time.append(i*step)
-
-# Position averages
+# Dislocation position averages
 avg = []
 
-
+# Read in simulation cell dimensions from a LAMMPS dump file
+# This can be any dump file in the trajectory
 x_lim, y_lim, z_lim = get_cell_lims("dump.shear_unwrap.0")
 
 for i, frame in enumerate(disloc_frames):
-
-    # Read in cell extents from dump file
     dislocations = read_disloc_data(frame)
-
     # Process dislocation data
     x_coords_i = []
     y_coords_i = []
@@ -44,19 +65,18 @@ for i, frame in enumerate(disloc_frames):
     avg_pos = []
 
     for dislocation in dislocations:
-        # Wrap coordinates
-        # dislocation = wrap_coords_y(dislocation,y_lim)
+        # Wrap coordinates in y
         dislocation = wrap_coords(dislocation,"y",y_lim)
 
-
         # Get coordinates of dislocation line vertices
+        # From 
         x, y, z = get_disloc_coords(dislocation)
         x_coords_i.append(x)
         y_coords_i.append(y)
         z_coords_i.append(z)
 
-        # Get average position
-        # Also save coordinates of all vertices
+        # Get the average position of a dislocation
+        # Also save the coordinates of all vertices
         avg_pos.append([get_avg_pos(dislocation),list(zip(x,y,z))])
 
     # Append coordinates
@@ -67,11 +87,7 @@ for i, frame in enumerate(disloc_frames):
     # Append average positions
     avg.append(np.array(avg_pos, dtype=object))
 
-###################################################################
-#                                                                 #
-#                      Dislocation Tracking                       #
-#                                                                 #
-###################################################################
+# Dislocation Tracking
 position, vertices, t = track_disloc(avg, x_lim)
 
 ###################################################################
@@ -84,8 +100,8 @@ position, vertices, t = track_disloc(avg, x_lim)
 partial_1 = position[0]
 partial_2 = position[1]
 
-partial_1_vel = get_velocity(partial_1, step)
-partial_2_vel = get_velocity(partial_2, step)
+partial_1_vel = get_velocity(partial_1, timestep)
+partial_2_vel = get_velocity(partial_2, timestep)
 
 labels = ["Partial 1", "Partial 2"]
 
@@ -115,7 +131,7 @@ fig.savefig("partials_pos_vel.png", dpi=350, format="png")
 perfect_disloc = perfect_disloc_coords(partial_1, partial_2)
 
 # Calculate perfect dislocation velocities
-velocity = get_velocity(perfect_disloc, step)
+velocity = get_velocity(perfect_disloc, timestep)
 
 fig1, axs = plt.subplots(1, 2 , figsize=(11,5))
 fig1.suptitle("Perfect Dislocation")
@@ -138,7 +154,7 @@ fig1.savefig("perfect_pos_vel.png", dpi=350, format="png")
 
 # Write coordinates and velocity of perfect dislocation
 filename = "perfect_pos.txt"
-write_prop(filename, perfect_disloc, time)
+write_prop(filename, perfect_disloc, sim_time)
 
 filename = "perfect_vel.txt"
-write_prop(filename, velocity, time)
+write_prop(filename, velocity, sim_time)
